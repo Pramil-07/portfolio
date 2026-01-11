2afac71a73dc071ef61fdc14b3ec555a3eccd01f
Pramil-07 <pramildhungana12345@gmail.com>
2026-01-11 16:38:28 +0545

    orginal code without three.js

--- src/components/models/tech_logos/TechIconCardExperience.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-const TechIconCardExperience = ({ model }) => {
-    return (_jsx("img", { src: model.modelPath, alt: model.name, className: "w-full h-full object-contain" }));
-};
-export default TechIconCardExperience;
--- src/secitons/LogoShowcase.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { logoIconsList } from "../constants";
-const LogoIcon = ({ icon }) => {
-    return (_jsx("div", { className: "flex-none flex-center marquee-item", children: _jsx("img", { src: icon.imgPath, alt: icon.name }) }));
-};
-const LogoShowcase = () => (_jsxs("div", { className: "md:my-20 my-10 relative", children: [_jsx("div", { className: "gradient-edge" }), _jsx("div", { className: "gradient-edge" }), _jsx("div", { className: "marquee h-52", children: _jsxs("div", { className: "marquee-box md:gap-12 gap-5", children: [logoIconsList.map((icon, index) => (_jsx(LogoIcon, { icon: icon }, index))), logoIconsList.map((icon, index) => (_jsx(LogoIcon, { icon: icon }, index)))] }) })] }));
-export default LogoShowcase;
